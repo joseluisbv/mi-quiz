c8761a2c7a7fdbf68064b37ab42b3a2b038b32ee
JOSE LUIS BALBOA <chelucky@gmail.com>
2015-08-01 14:03:16 +0100

    Busquedas 1 testeado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,9 +16,17 @@ exports.load = function(req, res, next, quizId) {
 
 //GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes});
-  }).catch(function(error) { next(error);})
+  if (req.query.search){
+    var condicion = "%" + req.query.search + "%";
+    models.Quiz.findAll(
+      {where: ["pregunta like ?", condicion]}).then(function(quizes){
+        res.render('quizes/index.ejs', {quizes: quizes});
+      }).catch(function(error) { next(error);})
+  } else {
+    models.Quiz.findAll().then(function(quizes) {
+      res.render('quizes/index.ejs', { quizes: quizes});
+    }).catch(function(error) { next(error);})
+  }  
 };
 
 // GET /quizes/:id
