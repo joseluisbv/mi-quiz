1974212d778d6a7037c1933e96cf7c9fec1ead3c
JOSE LUIS BALBOA <chelucky@gmail.com>
2015-08-02 12:09:17 +0100

    Crear pregunta

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//con urlencoded gestionamos los parámetros POST codificados en el body
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
