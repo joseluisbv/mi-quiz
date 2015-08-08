ea63919e7a1f39cd647e25cb262f5315d2aef0f7
JOSE LUIS BALBOA <chelucky@gmail.com>
2015-08-08 10:58:28 +0100

    Crear comentario

--- models/models.js
+++ models/models.js
@@ -26,10 +26,16 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     }
 );
 
-// Importar la definicion de la tabla Quiz en quiz.js
+// Importar la definicion de la tabla Quiz en quiz.js y exportamos objeto Quiz
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz = Quiz; // exportar objeto de la tabla Quiz
 
-exports.Quiz = Quiz; // exportar definición de tabla Quiz
+// Importar la definición de la tabla Comment en comment.js y exportamos objeto Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+Comment.belongsTo(Quiz); //relacion 1 Comment está asociado a un Quiz
+Quiz.hasMany(Comment); //relación 1 Quiz está asociado a N Comment
+exports.Comment = Comment; //exportamos objeto de la tabla Comment
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
